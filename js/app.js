e4c8e371f74494720ab5f2bd5a8cf471551561ef
Araf Al-Jami <clown1331@gmail.com>
2018-11-22 10:40:47 +0600

    added

--- js/app.js
+++ js/app.js
@@ -5,8 +5,25 @@
 
     var createButton = document.getElementById("createButton");
     
+    var span = document.getElementsByClassName("close")[0];
+
     createButton.onclick = function() {
         modal.style.display = "block";
     };
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    document.forms[0].onsubmit = function(event) {
+        event.preventDefault();
+        modal.style.display = "none";
+    }
 
 })();
